5e7a8f311e9bed23dbf193947b8cbfef96afbc97
VaninaLopetegui <vaninalopetegui@gmail.com>
2023-09-29 12:16:22 -0300

    Se terminó generador de gatitos incluyendo boton select para elegir cantidad de img de gatitos

--- js/main.js
+++ js/main.js
@@ -1,20 +1,35 @@
-
-
-const generarImg = () =>{
-    const URLAPI = "https://api.thecatapi.com/v1/images/search";
+const generarImg = (cantidad) => {
+    const API_KEY = "live_dRoZM0GeKHT6fN2zwtM0OTnz5nKaKmjipS8P9DBRmQCy2RB6cBX9gpEkIilAUSfC";
+    const URLAPI = `https://api.thecatapi.com/v1/images/search?api_key=${API_KEY}&limit=${cantidad}`;
     fetch(URLAPI)
-    .then(response => response.json())
-    .then(data => {
-            let imagenGatito = document.getElementById("imagenGatito");
-            imagenGatito.innerHTML = "";
+        .then(response => response.json())
+        .then(data => {
+            let imagenesGatitos = document.getElementById("imagenesGatitos");
+            imagenesGatitos.innerHTML = "";
             data.forEach(gatito => {
                 let contenido = `
-                    <img src="${gatito.url}" alt="">
+                <img src="${gatito.url}" alt="">
                 `;
-                imagenGatito.innerHTML += contenido;
+                imagenesGatitos.innerHTML += contenido;
             })
         })
         .catch(error => console.log(error));
 }
 
-const botonGenerar = document.getElementById("botonGenerar").addEventListener("click", generarImg);
+const selectCantidad = document.getElementById("cantidadGatitos");
+const botonGenerar = document.getElementById("botonGenerar");
+
+const actualizarCantidad = () => {
+    const cantidadSeleccionada = selectCantidad.value;
+    let cantidad = 1;
+    if (cantidadSeleccionada === "cincoGatitos") {
+        cantidad = 5;
+    } else if (cantidadSeleccionada === "MuchosGatitos") {
+        cantidad = 10;
+    }
+    generarImg(cantidad);
+};
+
+botonGenerar.addEventListener("click", actualizarCantidad);
+selectCantidad.addEventListener("change", actualizarCantidad);
+
